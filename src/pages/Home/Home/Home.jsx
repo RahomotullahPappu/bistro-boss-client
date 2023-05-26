ba339dd9bfe9730c2e77500b823cf97ad4c24531
RahomotullahPappu <rahomotullah72778@gmail.com>
2023-05-26 18:11:17 +0600

    bistro boss reasturent management project initial setup and create some routes design home pages using talwind css

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,22 @@
+import Banner from "../Banner/Banner";
+import Category from "../Category/Category";
+import Featured from "../Featured/Featured";
+import PopularMenu from "../PopulerMenu/PopulerMenu";
+import Testimonials from "../Testimoials/Testimonials";
+
+
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <Category></Category>
+            <PopularMenu></PopularMenu>
+            <Featured></Featured>
+            <Testimonials></Testimonials>
+            
+        </div>
+    );
+};
+
+export default Home;

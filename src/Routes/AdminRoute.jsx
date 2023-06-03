6d1ab17717bb0f47199e38e81d6d853b5748d2cd
RahomotullahPappu <rahomotullah72778@gmail.com>
2023-06-04 05:00:24 +0600

    added a admin dasboard add an item and manage items route created with admin route

--- src/Routes/AdminRoute.jsx
+++ src/Routes/AdminRoute.jsx
@@ -1,11 +1,12 @@
 
 import { Navigate, useLocation } from "react-router";
 import useAuth from "../hooks/useAuth";
+import useAdmin from "../hooks/useAdmin";
 
 
 const AdminRoute = ({ children }) => {
     const { user, loading } = useAuth();
-    const [isAdmin, isAdminLoading] = isAdmin();
+    const [isAdmin, isAdminLoading] = useAdmin();
     const location = useLocation();
 
     if(loading || isAdminLoading){
@@ -15,7 +16,7 @@ const AdminRoute = ({ children }) => {
     if (user && isAdmin ) {
         return children;
     }
-    return <Navigate to="/login" state={{from: location}} replace></Navigate>
+    return <Navigate to="/" state={{from: location}} replace></Navigate>
 };
 
 export default AdminRoute;
